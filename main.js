be691c5edf08c481334152bb196c656e8723cbf2
Your Name <ranamostafa2210@gmail.com@example.com>
2024-03-31 22:41:35 +0200

    add JS

---
+++ main.js
@@ -0,0 +1,212 @@
+const candies = ["Blue", "Orange", "Green", "Yellow", "Red", "Purple"];
+let board = [];
+const rows = 9;
+const columns = 9;
+let score = 0;
+
+let currentTile;
+let otherTile;
+window.onload = function () {
+  score = 0;
+  startGame();
+  // 1/10 of a second
+  setInterval(function () {
+    crushCandy();
+    slideCandy();
+    generateCandy();
+  }, 100);
+};
+
+function randomCandy() {
+  return candies[Math.floor(Math.random() * candies.length)]; // 0-5.99
+}
+
+function startGame() {
+  score = 0;
+  // initialize the board by generating random candies and placing them on the board
+  for (let r = 0; r < rows; r++) {
+    let row = [];
+    for (let c = 0; c < columns; c++) {
+      let tile = document.createElement("img");
+      tile.id = r.toString() + "." + c.toString();
+      tile.src = "./images/" + randomCandy() + ".png";
+
+      // Drag functionality
+      tile.addEventListener("dragstart", dragStart); // click on a candy, initialize drag process
+      tile.addEventListener("dragover", dragOver); //clicking on a candy, moving mouse to drag the candy
+      tile.addEventListener("dragenter", dragEnter); //dragging candy onto another candy
+      tile.addEventListener("dragleave", dragLeave); //leave candy onto another candy
+      tile.addEventListener("drop", dragDrop); // dropping candy over another candy
+      tile.addEventListener("dragend", dragEnd); // after drag process completed,we swap candies
+
+      document.getElementById("board").append(tile);
+      row.push(tile);
+    }
+    board.push(row);
+  }
+}
+
+function dragStart() {
+  // this refers to tile that was clicked on for dragging
+  currentTile = this;
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+}
+
+function dragLeave() {
+  console.log("dragleave");
+}
+
+function dragDrop() {
+  //this refers to the target tile that was dropped on
+  otherTile = this;
+}
+
+function dragEnd() {
+  if (currentTile.src.includes("blank") || otherTile.src.includes("blank")) {
+    return;
+  }
+
+  let currentCoords = currentTile.id.split("."); // id='0-0' => ['0','0']
+  let r = parseInt(currentCoords[0]);
+  let c = parseInt(currentCoords[1]);
+
+  let otherCoords = otherTile.id.split(".");
+  let r2 = parseInt(otherCoords[0]);
+  let c2 = parseInt(otherCoords[1]);
+
+  let moveLeft = c2 === c - 1 && r2 === r;
+  let moveRight = c2 === c + 1 && r2 === r;
+  let moveUp = r2 === r - 1 && c2 === c;
+  let moveDown = r2 === r + 1 && c2 === c;
+
+  let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
+  if (isAdjacent) {
+    let currentImg = currentTile.src;
+    let otherImg = otherTile.src;
+    currentTile.src = otherImg;
+    otherTile.src = currentImg;
+
+    let validMove = checkValid();
+    if (!validMove) {
+      let currentImg = currentTile.src;
+      let otherImg = otherTile.src;
+      currentTile.src = otherImg;
+      otherTile.src = currentImg;
+    }
+    return true;
+  }
+}
+
+function crushCandy() {
+  //crushFive()
+  //crushFour()
+  crushThree();
+  document.getElementById("score").innerText = score;
+}
+
+function crushThree() {
+  // check rows
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns - 2; c++) {
+      let candy1 = board[r][c];
+      let candy2 = board[r][c + 1];
+      let candy3 = board[r][c + 2];
+      if (
+        candy1.src === candy2.src &&
+        candy2.src === candy3.src &&
+        !candy1.src.includes("blank")
+      ) {
+        candy1.src = "./images/blank.png";
+        candy2.src = "./images/blank.png";
+        candy3.src = "./images/blank.png";
+        // dragStart() ? (score += 30) : (score = 0);
+        score += 30;
+      }
+    }
+  }
+  // check columns
+  for (let c = 0; c < columns; c++) {
+    for (let r = 0; r < rows - 2; r++) {
+      let candy1 = board[r][c];
+      let candy2 = board[r + 1][c];
+      let candy3 = board[r + 2][c];
+      if (
+        candy1.src === candy2.src &&
+        candy2.src === candy3.src &&
+        !candy1.src.includes("blank")
+      ) {
+        candy1.src = "./images/blank.png";
+        candy2.src = "./images/blank.png";
+        candy3.src = "./images/blank.png";
+        // dragStart() ? (score += 30) : (score = 0);
+        score += 30;
+      }
+    }
+  }
+}
+
+function checkValid() {
+  // check rows
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns - 2; c++) {
+      let candy1 = board[r][c];
+      let candy2 = board[r][c + 1];
+      let candy3 = board[r][c + 2];
+      if (
+        candy1.src === candy2.src &&
+        candy2.src === candy3.src &&
+        !candy1.src.includes("blank")
+      ) {
+        return true;
+      }
+    }
+  }
+  // check columns
+  for (let c = 0; c < columns; c++) {
+    for (let r = 0; r < rows - 2; r++) {
+      let candy1 = board[r][c];
+      let candy2 = board[r + 1][c];
+      let candy3 = board[r + 2][c];
+      if (
+        candy1.src === candy2.src &&
+        candy2.src === candy3.src &&
+        !candy1.src.includes("blank")
+      ) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+function slideCandy() {
+  // slide candy down
+  for (let c = 0; c < columns; c++) {
+    let index = rows - 1;
+    for (let r = columns - 1; r >= 0; r--) {
+      if (!board[r][c].src.includes("blank")) {
+        board[index][c].src = board[r][c].src;
+        index--;
+      }
+    }
+    for (let r = index; r >= 0; r--) {
+      board[r][c].src = "./images/blank.png";
+    }
+  }
+}
+
+function generateCandy() {
+  for (let c = 0; c < columns; c++) {
+    if (board[0][c].src.includes("blank")) {
+      board[0][c].src = "./images/" + randomCandy() + ".png";
+    }
+  }
+}
